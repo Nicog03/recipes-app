6b80ba3fd670d289e0b70a4c417b1aa838803f35
nicolas <nicolas.lima03@hotmail.com>
2023-11-21 19:05:46 -0300

    sets up the project folder

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Travel App",
+  description:
+    "An application that allows the user to browse and check different points of interests around the world.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
