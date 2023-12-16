0d4f862cc33add8f1f9e9899d3ab13a8533ff331
nicolas <nicolas.lima03@hotmail.com>
2023-12-16 13:41:10 -0300

    include new icon & modify start-page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Button from "./components/button";
+import Link from "next/link";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -10,13 +12,25 @@ const Home = () => {
       }}
       className="h-screen w-full px-8 py-11 flex flex-col justify-between"
     >
-      <h1 className=" text-7xl font-montserrat self-center">Travel App</h1>
+      <div className="flex gap-4 bg-white p-2 rounded-2xl w-fit items-center ">
+        <h1 className=" text-5xl font-montserrat font-bold text-right">
+          Travel <br />e App
+        </h1>
+        <Image
+          alt="location pin icon"
+          src={"/icons/location-pin.svg"}
+          width={70}
+          height={70}
+        ></Image>
+      </div>
       <div className="text-white font-montserrat flex flex-col gap-6">
         <p className="text-2xl">
           Plan your <br />
           <span className="text-5xl font-medium"> Luxurious Vacation</span>
         </p>
-        <Button text="Explore"></Button>
+        <Link href={"/initial"}>
+          <Button text="Explore"></Button>
+        </Link>
       </div>
     </div>
   );
