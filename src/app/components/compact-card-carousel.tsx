6bde69ab15994a2c5e3ad0676fb9d7ad7bc0e20d
nicolas <nicolas.lima03@hotmail.com>
2023-12-20 20:40:20 -0300

    create a component for the compact cards carousel

---
+++ src/app/components/compact-card-carousel.tsx
@@ -0,0 +1,30 @@
+import useEmblaCarousel from "embla-carousel-react";
+import CompactAttractionCard from "./compact-attraction-card";
+
+const CompactCardCarousel = () => {
+  const [compactCarousel] = useEmblaCarousel();
+
+  return (
+    <div className="embla mr-[-1.25rem]" ref={compactCarousel}>
+      <div className="embla__container">
+        <CompactAttractionCard
+          country="Netherlands"
+          title="Explore Amsterdam"
+          subtitle="Hot Deal"
+        />
+        <CompactAttractionCard
+          country="Netherlands"
+          title="Explore Amsterdam"
+          subtitle="Hot Deal"
+        />
+        <CompactAttractionCard
+          country="Netherlands"
+          title="Explore Amsterdam"
+          subtitle="Hot Deal"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CompactCardCarousel;
