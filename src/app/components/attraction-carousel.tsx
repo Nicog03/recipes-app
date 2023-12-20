14dc3831e7f49934119db4b39acee92d5410f7af
nicolas <nicolas.lima03@hotmail.com>
2023-12-20 19:54:48 -0300

    create the carousel component as well as an example locations array

---
+++ src/app/components/attraction-carousel.tsx
@@ -0,0 +1,38 @@
+import useEmblaCarousel from "embla-carousel-react";
+import AttractionCard from "./attraction-card";
+
+const examplePlaces = [
+  { name: "Amsterdam", rating: "4.7", imagePath: "/images/landscape.jpg" },
+  { name: "Delft", rating: "4.5", imagePath: "/images/landscape.jpg" },
+  { name: "Giethoorn", rating: "4.6", imagePath: "/images/landscape.jpg" },
+];
+
+interface CarouselTypes {
+  name: string;
+  rating: string;
+  imagePath: string;
+}
+
+const AttractionCarousel: React.FC<CarouselTypes> = ({
+  name,
+  rating,
+  imagePath,
+}) => {
+  const [carousel] = useEmblaCarousel();
+
+  return (
+    <div className="embla mr-[-1.25rem]" ref={carousel}>
+      <div className="embla__container">
+        {examplePlaces.map((place) => (
+          <AttractionCard
+            name={place.name}
+            rating={place.rating}
+            imagePath={place.imagePath}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AttractionCarousel;
