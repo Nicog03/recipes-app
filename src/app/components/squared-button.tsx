c067ef21e00572f216e39fb99d425044f369cc26
nicolas <nicolas.lima03@hotmail.com>
2024-01-09 21:09:38 -0300

    implent the return function of the button & add hover animation

--- src/app/components/squared-button.tsx
+++ src/app/components/squared-button.tsx
@@ -1,10 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 interface SquaredButtonInterface {
   imageUrl: string;
 }
 
 const SquaredButton: React.FC<SquaredButtonInterface> = ({ imageUrl }) => {
+  const router = useRouter();
+
   return (
-    <button className="bg-white h-10 w-10 flex items-center justify-center rounded-lg">
+    <button
+      type="button"
+      onClick={() => {
+        router.back();
+      }}
+      className="bg-white h-10 w-10 flex items-center justify-center rounded-lg hover:bg-gray-200 transition"
+    >
       <img src={imageUrl} alt="button icon" />
     </button>
   );
