a20b92deb002f22c6df009ca36e34f6cf8b093d9
nicolas <nicolas.lima03@hotmail.com>
2024-01-10 19:13:37 -0300

    applies the montserrat font to some headers

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -13,30 +13,28 @@ const Homepage = () => {
   const locationName = "Netherlands";
 
   return (
-    <>
+    <div className="p-5 flex flex-col gap-6">
       <NavBar />
-      <div className="p-5 flex flex-col gap-6">
-        <div className="flex justify-between items-start">
-          <HomepageTitle locationName={locationName} />
-          <LocationInput locationName={locationName} />
-        </div>
-        <SearchBar placeholder="Find things to do"></SearchBar>
-        <TagsCarousel />
-        <div className="flex flex-col gap-8">
-          <section className="flex flex-col gap-3">
-            <div className="flex justify-between">
-              <h1 className="font-semibold text-lg">Popular</h1>
-              <Link href={""}>See All</Link>
-            </div>
-            <AttractionCarousel />
-          </section>
-          <section className="flex flex-col gap-4">
-            <h1 className="font-semibold text-lg">Recommended</h1>
-            <CompactCardCarousel />
-          </section>
-        </div>
+      <div className="flex justify-between items-start">
+        <HomepageTitle locationName={locationName} />
+        <LocationInput locationName={locationName} />
       </div>
-    </>
+      <SearchBar placeholder="Find things to do"></SearchBar>
+      <TagsCarousel />
+      <div className="flex flex-col gap-8">
+        <section className="flex flex-col gap-3">
+          <div className="flex justify-between">
+            <h1 className="font-montserrat font-semibold text-lg">Popular</h1>
+            <Link href={""}>See All</Link>
+          </div>
+          <AttractionCarousel />
+        </section>
+        <section className="flex flex-col gap-4">
+          <h1 className="font-montserrat font-semibold text-lg">Recommended</h1>
+          <CompactCardCarousel />
+        </section>
+      </div>
+    </div>
   );
 };
 
