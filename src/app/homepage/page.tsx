c56a4605d73da9a7e7135fd14be86e1ce6b243ff
nicolas <nicolas.lima03@hotmail.com>
2024-01-11 17:33:37 -0300

    turns the recommended section into a component of its own

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import SearchBar from "../components/search-bar";
-import Link from "next/link";
 import LocationInput from "../components/location-input";
 import HomepageTitle from "../components/homepage-title";
 import NavBar from "../components/nav-bar";
-import CompactCardCarousel from "../components/molecules/compact-card-carousel";
 import TagsCarousel from "../components/tag-carousel";
 import PopularSection from "../components/molecules/popular-section";
+import RecommendedSection from "../components/molecules/recommended-section";
 
 const Homepage = () => {
   const locationName = "Netherlands";
@@ -19,15 +18,10 @@ const Homepage = () => {
         <HomepageTitle locationName={locationName} />
         <LocationInput locationName={locationName} />
       </div>
-      <SearchBar placeholder="Find things to do"></SearchBar>
+      <SearchBar placeholder="Find things to do" />
       <TagsCarousel />
       <PopularSection />
-      <div className="flex flex-col gap-8">
-        <section className="flex flex-col gap-4">
-          <h1 className="font-montserrat font-semibold text-lg">Recommended</h1>
-          <CompactCardCarousel />
-        </section>
-      </div>
+      <RecommendedSection />
     </div>
   );
 };
