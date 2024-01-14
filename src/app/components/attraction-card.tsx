1f0bc0c7bc3efd22be377bd34c6d0c32f652cff0
nicolas <nicolas.lima03@hotmail.com>
2024-01-13 23:25:51 -0300

    replace the old button element with the RoundedButton component

--- src/app/components/attraction-card.tsx
+++ src/app/components/attraction-card.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import AttractionTag from "./attraction-tag";
 import Link from "next/link";
+import RoundedButton from "./rounded-button";
 
 interface LocationType {
   name: string;
@@ -27,15 +28,11 @@ const AttractionCard: React.FC<LocationType> = ({
             <AttractionTag text={name} />
             <AttractionTag text={rating} iconUrl="/icons/star.svg" />
           </div>
-          <button className="h-6 w-6 border-[1px] rounded-full flex items-center justify-center">
-            <Image
-              className="invert"
-              alt="heart image"
-              src={"/icons/heart-lined.svg"}
-              height={16}
-              width={16}
-            ></Image>
-          </button>
+          <RoundedButton
+            imageUrl="/icons/heart-lined.svg"
+            height={24}
+            width={24}
+          />
         </div>
       </div>
     </Link>
