0186ed85618286d769ea4f6a7d876e22602fa2a5
nicolas <nicolas.lima03@hotmail.com>
2024-01-13 23:30:47 -0300

    modify how the rounded button is positioned

--- src/app/components/large-attraction-image.tsx
+++ src/app/components/large-attraction-image.tsx
@@ -12,9 +12,17 @@ const LargeAttractionImage = () => {
         alt="Image of the attraction"
         className="h-80 object-cover rounded-3xl"
       />
-      <RoundedButton imageUrl="/icons/heart-lined.svg" />
+      <span className="absolute right-3 -bottom-5 ">
+        <RoundedButton
+          imageUrl="/icons/heart-lined.svg"
+          height={32}
+          width={32}
+        />
+      </span>
     </div>
   );
 };
 
 export default LargeAttractionImage;
+
+// "absolute h-11 w-11  bg-gray-200 hover:bg-gray-300 transition rounded-full p-2";
