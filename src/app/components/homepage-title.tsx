f85047c24da91b69dbbf4db5bff8c405610ff573
nicolas <nicolas.lima03@hotmail.com>
2024-01-23 22:45:49 -0300

    include greeting message & remove location modification input

--- src/app/components/homepage-title.tsx
+++ src/app/components/homepage-title.tsx
@@ -1,12 +1,32 @@
+import Image from "next/image";
+
 interface TitleTypes {
-  locationName: string;
+  userName: string;
 }
 
-const HomepageTitle: React.FC<TitleTypes> = ({ locationName }) => {
+const HomepageTitle: React.FC<TitleTypes> = ({ userName }) => {
   return (
-    <div>
-      <p className="text-sm">Explore</p>
-      <h1 className="text-3xl font-medium">{locationName}</h1>
+    <div className="flex items-center justify-between">
+      <div className="flex gap-3">
+        <Image
+          alt="chef hat icon"
+          src={"icons/chef-hat-icon.svg"}
+          height={32}
+          width={32}
+        ></Image>
+        <div>
+          <p className="text-sm">Hello,</p>
+          <h1 className="text-3xl font-medium">{userName}</h1>
+        </div>
+      </div>
+      <button className="opacity-50 hover:opacity-100 transition">
+        <Image
+          src={"icons/help-icon.svg"}
+          alt="about this project"
+          width={32}
+          height={32}
+        ></Image>
+      </button>
     </div>
   );
 };
