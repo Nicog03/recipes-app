c2b51b6b98e22a19228c999ea7b28f30db2812be
nicolas <nicolas.lima03@hotmail.com>
2024-01-24 21:48:51 -0300

    change component name & slightly change the style

--- src/app/components/facility-item.tsx
+++ src/app/components/facility-item.tsx
@@ -1,17 +1,25 @@
-interface FacilityItemInterface {
+import Image from "next/image";
+
+interface InformationInterface {
   name: string;
   iconUrl: string;
 }
 
-const FacilityItem: React.FC<FacilityItemInterface> = ({ name, iconUrl }) => {
+const InformationItem: React.FC<InformationInterface> = ({ name, iconUrl }) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-gray-100 h-20 w-20 rounded-2xl gap-2">
-      <img src={iconUrl} alt="icon image" className="h-8 w-8" />
-      <p className="font-montserrat text-xs text-gray-400 whitespace-nowrap">
+    <div className="flex flex-col items-center justify-center bg-gray-100 h-20 w-20 rounded-2xl gap-1">
+      <Image
+        className="opacity-50"
+        src={iconUrl}
+        alt="information icon"
+        height={24}
+        width={24}
+      ></Image>
+      <p className="font-montserrat text-xs font-semibold text-center text-gray-400 ">
         {name}
       </p>
     </div>
   );
 };
 
-export default FacilityItem;
+export default InformationItem;
