ed87af279fb84432255141b794ed35b672ac319a
nicolas <nicolas.lima03@hotmail.com>
2024-01-25 23:00:24 -0300

    include new values on mock json & on card component

--- src/app/components/molecules/dishes-carousel.tsx
+++ src/app/components/molecules/dishes-carousel.tsx
@@ -3,17 +3,28 @@ import DishCard from "../dish-card";
 
 const exampleDishes = [
   {
-    name: "Pasta",
+    name: "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
     price: "$15",
     imagePath: "/images/food-bowl.jpg",
     id: "1",
+    dishType: "dinner",
+    isPopular: false,
   },
-  { name: "Tacos", price: "$12", imagePath: "/images/food-bowl.jpg", id: "2" },
+  {
+    name: "Tacos",
+    price: "$12",
+    imagePath: "/images/food-bowl.jpg",
+    id: "2",
+    dishType: "side dish",
+    isPopular: true,
+  },
   {
     name: "Fried Rice",
     price: "$20",
     imagePath: "/images/food-bowl.jpg",
     id: "3",
+    dishType: "dinner",
+    isPopular: true,
   },
 ];
 
@@ -26,9 +37,10 @@ const DishesCarousel = () => {
         {exampleDishes.map((dish) => (
           <DishCard
             name={dish.name}
-            rating={dish.price}
             imagePath={dish.imagePath}
             key={dish.id}
+            dishType={dish.dishType}
+            isPopular={dish.isPopular}
           />
         ))}
       </div>
