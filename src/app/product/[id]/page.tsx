a3487ea2c3ba9b8daa632a86db10191ea526ee9f
nicolas <nicolas.lima03@hotmail.com>
2024-02-01 18:39:24 -0300

    pass most of the api data as props to the page components & display loading component on data fetch

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -8,6 +8,7 @@ import IngredientsSection from "../../components/molecules/ingredients-section";
 import RecipeSection from "../../components/molecules/recipe-section";
 import { useQuery } from "@tanstack/react-query";
 import { getRecipe } from "../../../../api/api-requests";
+import LoadingDishPage from "@/app/components/loading-dish-page";
 
 const descText =
   "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs might be a good recipe to expand your main course repertoire. One portion of this dish contains approximately 19g of protein...";
@@ -18,25 +19,31 @@ const recipeText =
 const AttractionPage = ({ params }: any) => {
   const id = params.id;
 
-  const { data } = useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ["random-recipes"],
     queryFn: () => getRecipe(id),
     refetchOnWindowFocus: false,
   });
 
-  return (
-    <div className="p-5 flex flex-col gap-6 h-screen ">
-      <LargeAttractionImage />
-      <DescriptionHeading
-        title="Pasta with Garlic, Scallions, 
-Cauliflower & Breadcrumbs"
-        credits="Full Belly Sisters"
-      />
-      <DescriptionParagraph description={descText} />
-      <DishInformation />
-      <IngredientsSection />
-      <RecipeSection recipe={recipeText} />
-    </div>
+  console.log(data?.extendedIngredients);
+
+  return isFetching ? (
+    <LoadingDishPage />
+  ) : (
+    data && (
+      <div className="p-5 flex flex-col gap-6 h-screen ">
+        <LargeAttractionImage imageUrl={data.image} />
+        <DescriptionHeading
+          title={data.title}
+          credits={data.sourceName}
+          sourceUrl={data.sourceUrl}
+        />
+        <DescriptionParagraph description={data.summary} />
+        <DishInformation />
+        <IngredientsSection ingredients={data.extendedIngredients} />
+        <RecipeSection recipe={data.instructions} />
+      </div>
+    )
   );
 };
 
