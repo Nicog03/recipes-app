90c28d702518b40afb77efc74a239d802787f073
nicolas <nicolas.lima03@hotmail.com>
2024-02-01 16:34:35 -0300

    display loadingCard while the api is being fetched

--- src/app/components/molecules/popular-section.tsx
+++ src/app/components/molecules/popular-section.tsx
@@ -5,13 +5,22 @@ import Header from "../header-component";
 import DishesCarousel from "./dishes-carousel";
 import { getRandomRecipes } from "../../../../api/api-requests";
 import { useQuery } from "@tanstack/react-query";
+import DishCard from "../dish-card";
+import LoadingCard from "../loading-card";
+import { Island_Moments } from "next/font/google";
 
 const PopularSection = () => {
-  const { data } = useQuery({
+  const { data, isLoading, isFetching } = useQuery({
     queryKey: ["random-recipes"],
     queryFn: () => getRandomRecipes(),
-    staleTime: 10 * 1000,
+    // staleTime: 10 * 1000,
+    //implement this <---
+    refetchOnWindowFocus: false,
+    //--->
   });
+
+  if (isLoading) console.log("isLoading");
+  if (isFetching) console.log("isFetching");
 
   return (
     <section className="flex flex-col gap-3">
@@ -19,7 +28,7 @@ const PopularSection = () => {
         <Header text="Popular Dishes" size="small" />
         <TextButton text="See more" />
       </div>
-      <DishesCarousel recipes={data.recipes} />
+      {isFetching ? <LoadingCard /> : <DishesCarousel recipes={data.recipes} />}
     </section>
   );
 };
