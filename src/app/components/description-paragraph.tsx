4e149e12e74d44387cabc6b8cb0918418b3a71d8
nicolas <nicolas.lima03@hotmail.com>
2024-02-02 17:36:43 -0300

    implement fucntionality on 'read-more' button

--- src/app/components/description-paragraph.tsx
+++ src/app/components/description-paragraph.tsx
@@ -1,4 +1,5 @@
 import TextButton from "./text-button";
+import { useState } from "react";
 
 interface DescriptionParagraphInterface {
   description: string;
@@ -7,12 +8,29 @@ interface DescriptionParagraphInterface {
 const DescriptionParagraph: React.FC<DescriptionParagraphInterface> = ({
   description,
 }) => {
+  const [hidden, setHidden] = useState(true);
+
   return (
     <div className="flex flex-col gap-1">
-      <p className="text-gray-500 font-montserrat text-sm leading-4">
-        <div dangerouslySetInnerHTML={{ __html: description }}></div>
+      <p className="text-gray-500 font-montserrat text-sm leading-4 relative">
+        {hidden && (
+          <span
+            className="absolute left-0 right-0 top-0 bottom-0"
+            style={{
+              background: `linear-gradient(to bottom, transparent, transparent, white)`,
+            }}
+          />
+        )}
+        <div
+          dangerouslySetInnerHTML={{ __html: description }}
+          className={`${!hidden ? "h-fit" : "max-h-24"} ` + "overflow-hidden"}
+        ></div>
       </p>
-      <TextButton text="Read more" iconUrl="/icons/down-arrow.svg" />
+      <TextButton
+        text={`${hidden ? "Show more" : "Show less"}`}
+        iconUrl={`${hidden ? "/icons/down-arrow.svg" : "/icons/up-arrow.svg"}`}
+        onClick={() => setHidden((prev) => !prev)}
+      />
     </div>
   );
 };
