c06a14a0d6b26dd3058203c7b5c141baba071452
nicolas <nicolas.lima03@hotmail.com>
2024-02-02 17:38:01 -0300

    add new prop: onClick

--- src/app/components/text-button.tsx
+++ src/app/components/text-button.tsx
@@ -1,11 +1,21 @@
+import { MouseEventHandler } from "react";
+
 interface TextButtonInterface {
   text: string;
   iconUrl?: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-const TextButton: React.FC<TextButtonInterface> = ({ text, iconUrl }) => {
+const TextButton: React.FC<TextButtonInterface> = ({
+  text,
+  iconUrl,
+  onClick,
+}) => {
   return (
-    <button className="text-[--accent] hover:brightness-120 transition font-montserrat font-semibold text-sm flex whitespace-nowrap items-center gap-1">
+    <button
+      onClick={onClick}
+      className="text-[--accent] hover:brightness-120 transition font-montserrat font-semibold text-sm flex whitespace-nowrap items-center gap-1"
+    >
       {text} {iconUrl ? <img src={iconUrl} alt="icon image" /> : null}
     </button>
   );
