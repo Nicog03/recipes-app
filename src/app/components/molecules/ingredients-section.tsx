47134826c65595fa3117bf261b1445a693683382
nicolas <nicolas.lima03@hotmail.com>
2024-02-02 21:45:46 -0300

    include background image

--- src/app/components/molecules/ingredients-section.tsx
+++ src/app/components/molecules/ingredients-section.tsx
@@ -1,6 +1,7 @@
 import { IngredientType } from "../../../../api/api-requests";
 import Header from "../header-component";
 import IngredientEntry from "../ingredient-entry";
+import Image from "next/image";
 
 interface IngredientsSectionInterface {
   ingredients: IngredientType[];
@@ -14,20 +15,33 @@ const IngredientsSection: React.FC<IngredientsSectionInterface> = ({
   ingredients,
 }) => {
   return (
-    <div className="flex flex-col gap-1">
-      <Header text="Ingredients" size="small" />
-      <ul className="flex flex-col gap-3">
-        {ingredients.map((ingredient) => {
-          return (
-            <IngredientEntry
-              name={capitalizeFirstLetter(ingredient.originalName)}
-              amount={ingredient.amount}
-              unit={ingredient.unit}
-              key={ingredient.id}
-            />
-          );
-        })}
-      </ul>
+    <div className="relative ">
+      <div
+        className=" bg-gray-100 absolute top-0 right-0 bottom-0 left-0 -z-10 rounded-xl"
+        style={{
+          backgroundImage:
+            "url(/images/groceries.png), url(/images/pattern.jpg)",
+          backgroundSize: "60%, cover",
+          backgroundPosition: "120% 100%, top",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+
+      <div className="flex flex-col gap-1 p-2 rounded-xl">
+        <Header text="Ingredients" size="small" />
+        <ul className="flex flex-col gap-3">
+          {ingredients.map((ingredient) => {
+            return (
+              <IngredientEntry
+                name={capitalizeFirstLetter(ingredient.originalName)}
+                amount={ingredient.amount}
+                unit={ingredient.unit}
+                key={ingredient.id}
+              />
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
