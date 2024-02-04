a29b76b5f1d3b9d6345d0ab921a4dd7b4a378626
nicolas <nicolas.lima03@hotmail.com>
2024-02-03 23:31:43 -0300

    fix semantic error: a div was a p element parent

--- src/app/components/description-paragraph.tsx
+++ src/app/components/description-paragraph.tsx
@@ -12,7 +12,7 @@ const DescriptionParagraph: React.FC<DescriptionParagraphInterface> = ({
 
   return (
     <div className="flex flex-col gap-1">
-      <p className="text-gray-500 font-montserrat text-sm leading-4 relative">
+      <div className="text-gray-500 font-montserrat text-sm leading-4 relative">
         {hidden && (
           <span
             className="absolute left-0 right-0 top-0 bottom-0"
@@ -21,11 +21,11 @@ const DescriptionParagraph: React.FC<DescriptionParagraphInterface> = ({
             }}
           />
         )}
-        <div
+        <p
           dangerouslySetInnerHTML={{ __html: description }}
           className={`${!hidden ? "h-fit" : "max-h-24"} ` + "overflow-hidden"}
-        ></div>
-      </p>
+        ></p>
+      </div>
       <TextButton
         text={`${hidden ? "Show more" : "Show less"}`}
         iconUrl={`${hidden ? "/icons/down-arrow.svg" : "/icons/up-arrow.svg"}`}
