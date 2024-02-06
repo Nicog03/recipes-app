a10aaa732091545f54c9d3c6ef21d5dc631229f6
nicolas <nicolas.lima03@hotmail.com>
2024-02-06 17:44:54 -0300

    export capitalizeFirstLetter function

--- src/app/components/molecules/ingredients-section.tsx
+++ src/app/components/molecules/ingredients-section.tsx
@@ -6,7 +6,7 @@ interface IngredientsSectionInterface {
   ingredients: IngredientType[];
 }
 
-const capitalizeFirstLetter = (string: string) => {
+export const capitalizeFirstLetter = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
