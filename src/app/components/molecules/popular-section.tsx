fdf170830ab6006528bf30c153527dc7bdccdefe
nicolas <nicolas.lima03@hotmail.com>
2024-02-06 18:39:57 -0300

    remove unused components & set 10sec stale time on data

--- src/app/components/molecules/popular-section.tsx
+++ src/app/components/molecules/popular-section.tsx
@@ -5,28 +5,21 @@ import Header from "../header-component";
 import DishesCarousel from "./dishes-carousel";
 import { getRandomRecipes } from "../../../../api/api-requests";
 import { useQuery } from "@tanstack/react-query";
-import DishCard from "../dish-card";
 import LoadingCard from "../loading-card";
-import { Island_Moments } from "next/font/google";
 
 const PopularSection = () => {
-  const { data, isLoading, isFetching } = useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ["random-recipes"],
     queryFn: () => getRandomRecipes(),
-    // staleTime: 10 * 1000,
-    //implement this <---
+    staleTime: 10 * 1000,
     refetchOnWindowFocus: false,
-    //--->
   });
-
-  if (isLoading) console.log("isLoading");
-  if (isFetching) console.log("isFetching");
 
   return (
     <section className="flex flex-col gap-3">
       <div className="flex justify-between">
         <Header text="Popular Dishes" size="small" />
-        <TextButton text="See more" />
+        <TextButton onClick={() => {}} text="See more" />
       </div>
       {isFetching ? <LoadingCard /> : <DishesCarousel recipes={data.recipes} />}
     </section>
