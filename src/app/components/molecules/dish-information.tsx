0b0f5bb08b285e7d72714568d53990e9c1a8e568
nicolas <nicolas.lima03@hotmail.com>
2024-02-06 17:46:08 -0300

    include a new info item

--- src/app/components/molecules/dish-information.tsx
+++ src/app/components/molecules/dish-information.tsx
@@ -5,13 +5,19 @@ interface DishInformationInterface {
   readyInMinutes: number;
   servings: number;
   dairyFree: boolean;
+  isVegan: boolean;
+  isVegetarian: boolean;
 }
 
 const DishInformation: React.FC<DishInformationInterface> = ({
   readyInMinutes,
   dairyFree,
   servings,
+  isVegan,
+  isVegetarian,
 }) => {
+  console.log("vegetarian & vegan:", isVegetarian, isVegan);
+
   return (
     <div className="flex flex-col gap-1">
       <Header text="Dish Information" size="small" />
@@ -30,6 +36,12 @@ const DishInformation: React.FC<DishInformationInterface> = ({
             dairyFree ? "/icons/no-dairy-icon.svg" : "/icons/milk-icon.svg"
           }
         />
+        {(isVegan || isVegetarian) && (
+          <InformationItem
+            name={isVegan ? "Vegan" : isVegetarian ? "Vegetarian" : ""}
+            iconUrl="/icons/leaf-icon.svg"
+          />
+        )}
       </div>
     </div>
   );
