d2e71d724232a24e1f8d119ff959894e681f1676
nicolas <nicolas.lima03@hotmail.com>
2024-02-07 19:18:32 -0300

    slightly modify the component style & trigger navigation onSubmit

--- src/app/components/search-bar.tsx
+++ src/app/components/search-bar.tsx
@@ -1,17 +1,41 @@
+"use client";
+
+import { FormEvent } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+
 interface SearchBar {
   placeholder: string;
 }
 
 const SearchBar: React.FC<SearchBar> = ({ placeholder }) => {
+  const { push } = useRouter();
+
+  function navigate(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    push("/search");
+  }
+
   return (
     <>
-      <span className="flex items-center relative before:content-[url('/icons/Search.svg')] before:h-[20px] before:w-[20px] before:absolute before:z-10 before:left-4 h-14">
-        <input
-          type="text"
-          className="h-14 bg-gray-100 rounded-2xl w-full absolute z-0 ps-12 focus:border-2 focus:border-[--accent] focus:outline-none focus:ring-0"
-          placeholder={placeholder}
-        />
-      </span>
+      <form onSubmit={navigate}>
+        <span className="flex items-center relative h-14">
+          <input
+            type="text"
+            className="h-14 bg-gray-100 rounded-full w-full absolute p-2 pe-14 focus:border-2 focus:border-[--accent] focus:outline-none focus:ring-0"
+            placeholder={placeholder}
+          />
+          <button className="absolute right-2 text-sm text-white bg-[--accent] p-2 rounded-full hover:opacity-70 transition">
+            <Image
+              className="invert"
+              alt="magnifying glass icon"
+              src={"/icons/search-icon.svg"}
+              height={24}
+              width={24}
+            />
+          </button>
+        </span>
+      </form>
     </>
   );
 };
