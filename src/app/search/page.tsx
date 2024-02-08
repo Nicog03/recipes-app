6b49ebc1d8d829961f09fa44e626068495294126
nicolas <nicolas.lima03@hotmail.com>
2024-02-07 23:29:22 -0300

    page organization: structure search page layout

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getSearchResults } from "../../../api/api-requests";
+import CompactCard from "../components/compact-card";
+import Header from "../components/header-component";
+import SearchBar from "../components/search-bar";
+
+type ResultType = {
+  title: string;
+  image: string;
+};
+
 export default function SearchPage() {
-  return <h1>Search page</h1>;
+  const { data } = useQuery({
+    queryKey: ["search-results"],
+    queryFn: () => getSearchResults("beans"),
+    staleTime: 1000 * 60 * 10,
+  });
+
+  return (
+    data && (
+      <div className="ps-5 pe-5 pt-5 flex flex-col gap-2">
+        <Header size="large" text="Search" />
+        <SearchBar placeholder="Search for dishes" />
+        <div
+          className="p-5 flex flex-col gap-2 rounded-ss-2xl rounded-se-2xl"
+          style={{
+            backgroundImage: "url(/images/pattern.jpg)",
+            backgroundSize: "cover",
+          }}
+        >
+          <Header size="medium" text="Results for Beans" />
+          {data.results.map((result: ResultType) => (
+            <CompactCard dishTitle={result.title} imageUrl={result.image} />
+          ))}
+        </div>
+      </div>
+    )
+  );
 }
