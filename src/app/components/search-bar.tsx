486e02d45bdca16fcf43e6cb0905eb13823fead4
nicolas <nicolas.lima03@hotmail.com>
2024-02-10 12:07:54 -0300

    add basic form validation logic to search-bar

--- src/app/components/search-bar.tsx
+++ src/app/components/search-bar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
@@ -9,27 +9,46 @@ interface SearchBar {
 }
 
 const SearchBar: React.FC<SearchBar> = ({ placeholder }) => {
+  const [isValid, setIsValid] = useState(true);
+
   const { push } = useRouter();
 
   function navigate(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(e.target);
     const inputValue: string = (
       (e.target as HTMLFormElement)[0] as HTMLInputElement
     ).value;
-    push(`/search?term=${inputValue}`);
+
+    if (inputValue) {
+      setIsValid(true);
+      push(`/search?term=${inputValue}`);
+    } else {
+      setIsValid(false);
+    }
   }
 
   return (
     <>
       <form onSubmit={navigate}>
         <span className="flex items-center relative h-14">
+          {!isValid && (
+            <p className="absolute text-sm -bottom-5 left-3 font-montserrat text-red-400 font-semibold">
+              Invalid value
+            </p>
+          )}
           <input
             type="text"
-            className="h-14 bg-gray-100 rounded-full w-full absolute p-2 pe-14 focus:border-2 focus:border-[--accent] focus:outline-none focus:ring-0"
+            className={`${
+              !isValid && "border-2 border-red-400"
+            } h-14 bg-gray-100 rounded-full w-full absolute p-2 pe-14 focus:border-2 focus:border-[--accent] focus:outline-none focus:ring-0`}
             placeholder={placeholder}
+            onFocus={() => setIsValid(true)}
           />
-          <button className="absolute right-2 text-sm text-white bg-[--accent] p-2 rounded-full hover:opacity-70 transition">
+          <button
+            className={`${
+              !isValid && "saturate-0"
+            } absolute right-2 text-sm text-white bg-[--accent] p-2 rounded-full hover:opacity-70 transition`}
+          >
             <Image
               className="invert"
               alt="magnifying glass icon"
