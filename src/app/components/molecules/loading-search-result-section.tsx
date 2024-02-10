e893d7ed6a90dc42687f9d1c30a68d53624b3ed6
nicolas <nicolas.lima03@hotmail.com>
2024-02-10 13:41:24 -0300

    remove bg color

--- src/app/components/molecules/loading-search-result-section.tsx
+++ src/app/components/molecules/loading-search-result-section.tsx
@@ -3,14 +3,15 @@ import LoadingCompactCard from "../loading-compact-card";
 export default function LoadingSearchResultSection() {
   return (
     <div
-      className="p-5 flex flex-col gap-2 rounded-ss-2xl rounded-se-2xl bg-gray-100 h-full relative
+      className="p-5 flex flex-col gap-2 rounded-ss-2xl rounded-se-2xl  h-full relative
     before:absolute before:inset-0
 before:-translate-x-full
 before:animate-[shimmer_2s_infinite]
 before:bg-gradient-to-r
 before:from-transparent before:via-white before:to-transparent
 isolate
-    overflow-hidden"
+    overflow-hidden
+    "
     >
       <div className="h-8 w-3/4 bg-gray-300 rounded-full" />
       <LoadingCompactCard />
