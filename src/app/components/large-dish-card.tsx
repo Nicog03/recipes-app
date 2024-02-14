5cfc2a34f51a19e03b79b5f7bf52afd3690431c8
nicolas <nicolas.lima03@hotmail.com>
2024-02-14 19:01:19 -0300

    component style change: large dish card

--- src/app/components/large-dish-card.tsx
+++ src/app/components/large-dish-card.tsx
@@ -15,17 +15,24 @@ export default function LargeDishCard({
   return (
     <Link
       href={`/product/${id}`}
-      className="w-full h-52 flex flex-col bg-gray-100 rounded-2xl"
+      className="w-full h-52 flex flex-col bg-gray-100 rounded-2xl relative"
     >
-      <div className="relative w-full h-full">
-        <Image
-          alt="dish image"
-          src={image}
-          fill={true}
-          className="object-cover rounded-2xl"
-        />
-      </div>
-      <p className="font-montserrat font-semibold p-2">{title}</p>
+      <div
+        className="absolute left-0 top-0 right-0 bottom-0 z-10 rounded-2xl"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, transparent, transparent, black)",
+        }}
+      />
+      <Image
+        alt="dish image"
+        src={image}
+        fill={true}
+        className="object-cover rounded-2xl"
+      />
+      <p className="font-montserrat font-semibold p-2 absolute text-white bottom-0 left-0 z-20">
+        {title}
+      </p>
     </Link>
   );
 }
