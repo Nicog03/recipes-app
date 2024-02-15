913ec0217e103c7a73d961872b52d16adc6f86f5
nicolas <nicolas.lima03@hotmail.com>
2024-02-15 12:48:59 -0300

    create isFetching logic on popular dishes page

--- src/app/dishes/page.tsx
+++ src/app/dishes/page.tsx
@@ -5,9 +5,10 @@ import Header from "../components/header-component";
 import SquaredButton from "../components/squared-button";
 import { useQuery } from "@tanstack/react-query";
 import { getRandomRecipes } from "../../../api/api-requests";
+import LoadingMediumCardGrid from "../components/molecules/loading-medium-card-grid";
 
 export default function DishesPage() {
-  const { data } = useQuery({
+  const { data, isFetching } = useQuery({
     queryKey: ["popular-recipes"],
     queryFn: () => getRandomRecipes("10"),
     refetchOnWindowFocus: false,
@@ -15,14 +16,16 @@ export default function DishesPage() {
   });
 
   return (
-    data && (
-      <div className="flex flex-col gap-2 items-center p-5">
-        <div className="flex gap-2 bg-slate-100 p-2 rounded-xl items-center w-full ">
-          <SquaredButton imageUrl="/icons/left-caret-icon.svg" />
-          <Header size="large" text="Popular Dishes" />
-        </div>
-        <PopularDishesGrid recipes={data.recipes} />
+    <div className="flex flex-col gap-2 items-center p-5">
+      <div className="flex gap-2 bg-slate-100 p-2 rounded-xl items-center w-full ">
+        <SquaredButton imageUrl="/icons/left-caret-icon.svg" />
+        <Header size="large" text="Popular Dishes" />
       </div>
-    )
+      {isFetching ? (
+        <LoadingMediumCardGrid />
+      ) : (
+        data && <PopularDishesGrid recipes={data.recipes} />
+      )}
+    </div>
   );
 }
