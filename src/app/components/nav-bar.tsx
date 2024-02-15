5049d143e6418a0d7e9ede25821b12d3147289ff
nicolas <nicolas.lima03@hotmail.com>
2024-02-15 11:48:46 -0300

    include dynamic style on navbar

--- src/app/components/nav-bar.tsx
+++ src/app/components/nav-bar.tsx
@@ -1,18 +1,35 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const NavBar = () => {
+  const pathName = usePathname();
+
+  console.log(pathName);
+
   return (
     <div className=" bg-white w-full flex items-center gap-12 justify-center h-16 fixed bottom-0 left-0 py-6 rounded-t-[2rem] border-2 border-gray-200 z-10">
-      <Link href={"#"} className="h-fit rounded-full p-2">
+      <Link
+        href={"#"}
+        className={
+          `${pathName === "/" ? "bg-[--accent] " : ""}` +
+          "h-fit rounded-full p-2"
+        }
+      >
         <Image
           src={"/icons/home-icon.svg"}
           alt="heart icon"
           width={24}
           height={24}
+          className={pathName === "/" ? "brightness-200" : ""}
         />
       </Link>
-      <Link href={"#"}>
+      <Link
+        href={"#"}
+        className="h-fit rounded-full p-2 hover:bg-gray-100 transition"
+      >
         <Image
           src={"/icons/heart-icon.svg"}
           alt="heart icon"
@@ -20,7 +37,10 @@ const NavBar = () => {
           height={24}
         />
       </Link>
-      <Link href={"#"}>
+      <Link
+        href={"#"}
+        className="h-fit rounded-full p-2 hover:bg-gray-100 transition"
+      >
         <Image
           src={"/icons/profile-icon.svg"}
           alt="heart icon"
